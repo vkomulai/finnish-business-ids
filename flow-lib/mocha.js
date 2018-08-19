998643f66ace003cefa32c0a6a18058312d86f76
Ville Komulainen <ville.komulainen@wunderdog.fi>
2018-08-19 15:11:15 +0300

    Remove flow and eslint because using typescript

--- flow-lib/mocha.js
+++
@@ -1,7 +0,0 @@
-declare function describe(name: string, fn: (done: (err: any) => void) => ?Promise): void;
-declare function before(fn: (done: (err: any) => void) => ?Promise): void;
-declare function beforeEach(fn: (done: (err: any) => void) => ?Promise): void;
-declare function after(fn: (done: (err: any) => void) => ?Promise): void;
-declare function afterEach(fn: (done: (err: any) => void) => ?Promise): void;
-declare function it(name: string, fn: (done: (err: any) => void) => ?Promise): void;
-declare function xit(name: string, fn: (done: (err: any) => void) => ?Promise): void;
